e578cf21349b427e927cac83c9a271666e179059
miheeshin110-a11y <miheeshin110@gmail.com>
2026-06-10 16:02:45 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
 // ============================================================
 
 const CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/여기에_본인_배포URL/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbwV6hZn1j1oH8B8elpBc_P8PgtGxQjylSc_PmH8dwbXBxVGJ6VSt25LWBxTyhV25PpN/exec',
   APP_NAME: '발주단가 관리 시스템',
   VERSION: '1.0.0'
 };
